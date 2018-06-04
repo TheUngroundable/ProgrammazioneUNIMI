26aa82909dad508003d04d50aa43e8b162f5cdb8
TheUngroundable <arduiniumberto@gmail.com>
2018-06-04 22:17:18 +0200

    more work

--- pappagallo.java
+++ pappagallo.java
@@ -1,9 +1,34 @@
 
+import java.util.*;
+import java.io.*;
+
 public class pappagallo{
 
 	public static void main(String[] args) {
 	
-		System.out.println("babbowuedra");
+		Scanner in = new Scanner(System.in);
+
+		String pappagallo;
+		char exitcode;
+
+		do{
+
+			in.nextLine();
+			System.out.println("Inserisci una parola da far ripetere al pappagallo");
+
+			pappagallo = in.nextLine();
+
+			System.out.println(pappagallo);
+
+			System.out.println("Vuoi far parlare di nuovo il pappagallo? Y/N");
+
+			exitcode = in.next().charAt(0);
+
+			Character.toUpperCase(exitcode);
+
+		}while(exitcode != 'N');
+
+		System.out.println("Pappagallo!!!");
 
 	}
 
