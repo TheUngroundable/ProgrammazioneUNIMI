3c36a451b4c228aa1f6beda5f73cde46ce18f5af
TheUngroundable <arduiniumberto@gmail.com>
2018-06-08 21:08:19 +0200

    Incompleto HMMMMM

---
+++ Esercizi Laboratorio/130217/S3ToInt.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+//Chiudi il buffer con CTRL-Z
+
+public class S3ToInt{
+
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		String numero = in.next();
+		int output = 0;
+
+		for(int e = 0; e<numero.length(); e++){
+
+			for(int i = numero.length()-1; i>= 0; i--){
+
+				switch(numero.charAt(i)){
+
+					case 'd':
+
+						output += 2 * (Math.pow(3, e));
+
+					break;
+
+					case 'u':
+
+						output += 1 * (Math.pow(3, e));
+
+					break;
+
+					case 'z':
+
+						output += 0 * (Math.pow(3, e));
+
+					break;
+				}				
+			}
+		}
+
+		System.out.println(output);
+
+	}
+	
+}
