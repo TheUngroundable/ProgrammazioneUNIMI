f0d3c2184e1228fa8f08f9943aa5efde2ff0feaa
TheUngroundable <35199075+TheUngroundable@users.noreply.github.com>
2018-06-17 12:49:04 +0200

    Sistemato un po' di cose

---
+++ Scritto/Esercizi Scritto/RiconoscimentoLinguistico1/Tester.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Tester{
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+
+		try{
+
+			Scanner file = new Scanner(new FileReader("mix-k.txt"));
+
+
+		}catch(FileNotFoundException e){
+
+
+
+		}
+
+		ModelloLinguistico modello1 = new ModelloLinguistico();
+
+		ModelloLinguistico modello2 = new ModelloLinguistico(testo);
+
+	}
+	
+}
